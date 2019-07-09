0acc11a082f9ec5abda1df6ef4686980a3797be6
Jeffrey Wiedemann <jeffreywiedemann@dhcp-129-105-105-203.it.northwestern.edu>
2019-07-09 16:25:59 -0500

    looping through directory and multiple sheets working

---
+++ rollup_loop_dir.py
@@ -0,0 +1,36 @@
+#!/anaconda3/bin/python
+
+# https://automatetheboringstuff.com/chapter12/
+
+# import load_workbook
+from openpyxl import load_workbook
+
+
+import os
+
+scratch_dir = "/Users/jeffreywiedemann/Desktop/Resource_Planning/"
+
+for filename in os.listdir(scratch_dir):
+   if filename.endswith(".xlsx"):
+
+     #Uncomment below to see filename during testing. Testing?
+     #print (scratch_dir + filename)
+
+     # set file path
+     #filepath="/Users/jeffreywiedemann/Desktop/Resource_Planning/jeff_for_python.xlsx"
+     filepath = scratch_dir + filename
+     
+     wb=load_workbook(filepath)
+     
+     sheet = wb['Summary']
+     
+     A2=sheet['A2']
+     A1=sheet['A1']
+     
+     print(A2.value, end=",")
+     print(A1.value, end=",")
+
+     sheet = wb['Meetings & Admin']
+
+     M_B10=sheet['B10']
+     print(M_B10.value)
