bd10d82d3c08535f44f2263bcca10096efbe1fc3
Jeffrey Wiedemann <jeffreywiedemann@dhcp-129-105-105-203.it.northwestern.edu>
2019-07-09 14:56:07 -0500

    created rollup2.py with openpyxl module

--- rollup.py
+++ rollup.py
@@ -1,11 +1,19 @@
+#!/anaconda3/bin/python
+
 import xlrd
 
-//Open work book
-workbook = xlrd.open_workbook('/Users/jeffreywiedemann/Desktop/Resource_Planningjeff_for_python.xlsx')
+workbook = xlrd.open_workbook('/Users/jeffreywiedemann/Desktop/Resource_Planning/jeff_for_python.xlsx')
 
 worksheet = workbook.sheet_by_name('Summary')
 
-emp = worksheet.cell(0, 1).value
+emp = worksheet.cell(1, 0).value
 emp_group = worksheet.cell(0, 0).value
 emp_work_weeks = worksheet.cell(16, 2).value
-emp_project_houes = worksheet.cell(39, 2).value
+emp_project_hours = worksheet.cell(39, 2).value
+
+print (emp)
+print (emp_group)
+print (emp_work_weeks)
+print (emp_project_hours)
+
+
