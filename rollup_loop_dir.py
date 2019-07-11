4ef5a466e918b023b0c6c7bf75cae94910bd1699
Jeffrey Wiedemann <jeffreywiedemann@dhcp-129-105-105-203.it.northwestern.edu>
2019-07-11 11:06:50 -0500

    added Project Hours by group

--- rollup_loop_dir.py
+++ rollup_loop_dir.py
@@ -21,7 +21,7 @@ for filename in os.listdir(scratch_dir):
      #filepath="/Users/jeffreywiedemann/Desktop/Resource_Planning/jeff_for_python.xlsx"
      filepath = scratch_dir + filename
      
-     wb=load_workbook(filepath)
+     wb=load_workbook(filepath, data_only=True)
      
      sheet = wb['Summary']
      
@@ -30,10 +30,19 @@ for filename in os.listdir(scratch_dir):
      
      Total_Time_Away = sheet['C16']
      Total_General_Admin = sheet['C22']
-     Total_Managerial_Admin Time = sheet['C25']
+     Total_Managerial_Admin_Time = sheet['C25']
      Total_Support = sheet['C33']
      Total_Consulting = sheet['C37']
-     Hours_per_year_available_for_projects = sheet['C44'] 
+     Total_Other = sheet['C44'] 
+
+     Annual_CI_Project_Hours = sheet['C50']
+     Annual_AS_Project_Hours = sheet['C51']
+     Annual_IT_SS_Project_Hours = sheet['C52']
+     Annual_ISO_Project_Hours = sheet['C53']
+     Annual_Schools_or_Depts_Project_Hours = sheet['C54']
+
+
+
 
      print(A2.value, end=",")
      csvfile.write(A2.value + ",")
@@ -42,11 +51,48 @@ for filename in os.listdir(scratch_dir):
      print(A1.value, end=",")
      csvfile.write(A1.value + ",")
 
-     sheet = wb['Meetings & Admin']
+     print(Total_Time_Away.value, end=",")
+     csvfile.write(str(Total_Time_Away.value) + ",")
 
-     M_B10=sheet['B10']
+     print(Total_General_Admin.value, end=",")
+     csvfile.write(str(Total_General_Admin) + ",")
 
-     print(M_B10.value)
-     csvfile.write(str(M_B10.value) + ",")
+     print(Total_Managerial_Admin_Time.value, end=",")
+     csvfile.write(str(Total_Managerial_Admin_Time.value) + ",")
+
+     print(Total_Support.value, end=",")
+     csvfile.write(str(Total_Support.value) + ",")
+
+     print(Total_Consulting.value, end=",")
+     csvfile.write(str(Total_Consulting.value) + ",")
+
+     print(Total_Other.value, end=",")
+     csvfile.write(str(Total_Other.value) + ",")
+
+
+     print(Annual_CI_Project_Hours.value, end=",")
+     csvfile.write(str(Annual_CI_Project_Hours.value) + ",")
+
+     print(Annual_AS_Project_Hours.value, end=",")
+     csvfile.write(str(Annual_AS_Project_Hours.value) + ",")
+
+     print(Annual_IT_SS_Project_Hours.value, end=",")
+     csvfile.write(str(Annual_IT_SS_Project_Hours.value) + ",")
+
+     print(Annual_ISO_Project_Hours.value, end=",")
+     csvfile.write(str(Annual_ISO_Project_Hours.value) + ",")
+
+     print(Annual_Schools_or_Depts_Project_Hours.value, end=",")
+     csvfile.write(str(Annual_Schools_or_Depts_Project_Hours.value) + ",")
+
+
+
+
+#     sheet = wb['Meetings & Admin']
+#
+#     M_B10=sheet['B10']
+#
+#     print(M_B10.value)
+#     csvfile.write(str(M_B10.value) + ",")
 
      csvfile.write("\n")
