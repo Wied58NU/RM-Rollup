91e55796b36472ebce110447580bad70a99b8f6d
Jeff Wiedemann <jeff@jeffwiedemann.com>
2019-11-01 16:53:25 -0500

    added TOTAL_Admin, TOTAL_Project Hours, and Sums

--- rollup_loop_dir.py
+++ rollup_loop_dir.py
@@ -24,9 +24,25 @@ scratch_dir = "/Users/jeffreywiedemann/Desktop/Resource_Planning/Team_Reports/" 
 csvfile = open("/Users/jeffreywiedemann/Desktop/Resource_Planning/Team_Reports/" + team.upper() + "/" + team.lower() + "_rollup.csv","w+")
 
 
-print("name,dept,Total_Time_Away,Total_General_Admin,Total_Managerial_Admin_Time,Total_Support,Total_Consulting,Total_Other,Annual_CI_Project_Hours,Annual_AS_Project_Hours,Annual_IT_SS_Project_Hours,Annual_ISO_Project_Hours,Annual_Schools_or_Depts_Project_Hours")
+print("name,dept,Total_Time_Away,Total_General_Admin,Total_Managerial_Admin_Time,TOTAL_Admin_Time,Total_Support,Total_Consulting,Total_Other,Annual_CI_Project_Hours,Annual_AS_Project_Hours,Annual_IT_SS_Project_Hours,Annual_ISO_Project_Hours,Annual_Schools_or_Depts_Project_Hours,TOTAL_Project_Hours")
 
-csvfile.write("name,dept,Total_Time_Away,Total_General_Admin,Total_Managerial_Admin_Time,Total_Support,Total_Consulting,Total_Other,Annual_CI_Project_Hours,Annual_AS_Project_Hours,Annual_IT_SS_Project_Hours,Annual_ISO_Project_Hours,Annual_Schools_or_Depts_Project_Hours\n")
+csvfile.write("name,dept,Total_Time_Away,Total_General_Admin,Total_Managerial_Admin_Time,TOTAL_Admin_Time,Total_Support,Total_Consulting,Total_Other,Annual_CI_Project_Hours,Annual_AS_Project_Hours,Annual_IT_SS_Project_Hours,Annual_ISO_Project_Hours,Annual_Schools_or_Depts_Project_Hours,TOTAL_Project_Hours\n")
+
+People = 0
+Sum_Total_Time_Away = 0
+Sum_Total_General_Admin = 0
+Sum_Total_Managerial_Admin_Time = 0
+Sum_TOTAL_Admin_Time = 0
+Sum_Total_Support = 0
+Sum_Total_Consulting = 0
+Sum_Total_Other = 0
+Sum_Annual_CI_Project_Hours = 0
+Sum_Annual_AS_Project_Hours = 0
+Sum_Annual_IT_SS_Project_Hours = 0
+Sum_Annual_ISO_Project_Hours = 0
+Sum_Annual_Schools_or_Depts_Project_Hours = 0
+Sum_TOTAL_Project_Hours = 0
+
 
 for filename in os.listdir(scratch_dir):
    if filename.endswith(".xlsx"):
@@ -48,6 +64,7 @@ for filename in os.listdir(scratch_dir):
      Total_Time_Away = sheet['C16']
      Total_General_Admin = sheet['C22']
      Total_Managerial_Admin_Time = sheet['C25']
+     TOTAL_Admin_Time = sheet['C22'].value + sheet['C25'].value
      Total_Support = sheet['C33']
      Total_Consulting = sheet['C37']
      Total_Other = sheet['C41'] 
@@ -57,13 +74,27 @@ for filename in os.listdir(scratch_dir):
      Annual_IT_SS_Project_Hours = sheet['C52']
      Annual_ISO_Project_Hours = sheet['C53']
      Annual_Schools_or_Depts_Project_Hours = sheet['C54']
+     TOTAL_Project_Hours = sheet['C44']
 
 
+     Sum_Total_Time_Away = Sum_Total_Time_Away + Total_Time_Away.value
+     Sum_Total_General_Admin = Sum_Total_General_Admin + Total_General_Admin.value
+     Sum_Total_Managerial_Admin_Time = Sum_Total_Managerial_Admin_Time + Total_Managerial_Admin_Time.value
+     Sum_TOTAL_Admin_Time = Sum_TOTAL_Admin_Time + TOTAL_Admin_Time
+     Sum_Total_Support = Sum_Total_Support + Total_Support.value
+     Sum_Total_Consulting = Sum_Total_Consulting + Total_Consulting.value
+     Sum_Total_Other = Sum_Total_Other + Total_Other.value
+     Sum_Annual_CI_Project_Hours = Sum_Annual_CI_Project_Hours + Annual_CI_Project_Hours.value
+     Sum_Annual_AS_Project_Hours = Sum_Annual_AS_Project_Hours + Annual_AS_Project_Hours.value
+     Sum_Annual_IT_SS_Project_Hours = Sum_Annual_IT_SS_Project_Hours + Annual_IT_SS_Project_Hours.value
+     Sum_Annual_ISO_Project_Hours = Sum_Annual_ISO_Project_Hours + Annual_ISO_Project_Hours.value
+     Sum_Annual_Schools_or_Depts_Project_Hours = Sum_Annual_Schools_or_Depts_Project_Hours + Annual_Schools_or_Depts_Project_Hours.value
+     Sum_TOTAL_Project_Hours = Sum_TOTAL_Project_Hours + TOTAL_Project_Hours.value
 
+     People = People + 1
 
      print(Name.value, end=",")
      csvfile.write(Name.value + ",")
-
 
      print(Dept.value, end=",")
      csvfile.write(Dept.value + ",")
@@ -77,6 +108,9 @@ for filename in os.listdir(scratch_dir):
      print(Total_Managerial_Admin_Time.value, end=",")
      csvfile.write(str(Total_Managerial_Admin_Time.value) + ",")
 
+     print(TOTAL_Admin_Time, end=",")
+     csvfile.write(str(TOTAL_Admin_Time) + ",")
+
      print(Total_Support.value, end=",")
      csvfile.write(str(Total_Support.value) + ",")
 
@@ -85,7 +119,6 @@ for filename in os.listdir(scratch_dir):
 
      print(Total_Other.value, end=",")
      csvfile.write(str(Total_Other.value) + ",")
-
 
      print(Annual_CI_Project_Hours.value, end=",")
      csvfile.write(str(Annual_CI_Project_Hours.value) + ",")
@@ -99,8 +132,11 @@ for filename in os.listdir(scratch_dir):
      print(Annual_ISO_Project_Hours.value, end=",")
      csvfile.write(str(Annual_ISO_Project_Hours.value) + ",")
 
-     print(Annual_Schools_or_Depts_Project_Hours.value)
-     csvfile.write(str(Annual_Schools_or_Depts_Project_Hours.value))
+     print(Annual_Schools_or_Depts_Project_Hours.value, end=',')
+     csvfile.write(str(Annual_Schools_or_Depts_Project_Hours.value) + ",")
+
+     print(TOTAL_Project_Hours.value)
+     csvfile.write(str(TOTAL_Project_Hours.value))
 
 
 
@@ -116,9 +152,41 @@ for filename in os.listdir(scratch_dir):
 
 
 # end of for filename in os.listdir(scratch_dir):
+# print(Sum_Total_Time_Away)
+
+
+csvfile.write("TOTAL,,")
+
+csvfile.write(str(Sum_Total_Time_Away) + ",")
+
+csvfile.write(str(Sum_Total_General_Admin) + ",")
+
+csvfile.write(str(Sum_Total_Managerial_Admin_Time) + ",")
+
+csvfile.write(str(Sum_TOTAL_Admin_Time) + ",")
+
+csvfile.write(str(Sum_Total_Support) + ",")
+
+csvfile.write(str(Sum_Total_Consulting) + ",")
+
+csvfile.write(str(Sum_Total_Other) + ",")
+
+csvfile.write(str(Sum_Annual_CI_Project_Hours) + ",")
+
+csvfile.write(str(Sum_Annual_AS_Project_Hours) + ",")
+
+csvfile.write(str(Sum_Annual_IT_SS_Project_Hours) + ",")
+
+csvfile.write(str(Sum_Annual_ISO_Project_Hours) + ",")
+
+csvfile.write(str(Sum_Annual_Schools_or_Depts_Project_Hours) + ",")
+
+csvfile.write(str(Sum_TOTAL_Project_Hours))
 
 print()
-print("If there are errors, most likey there are file besides the RM Workbooks")
+print(People)
+print()
+print("If there are errors, most likey there are xlxs files besides the RM Workbooks")
 print()
 print("Do yourself a favor and only copy the output csv file back to Box")
 print()
